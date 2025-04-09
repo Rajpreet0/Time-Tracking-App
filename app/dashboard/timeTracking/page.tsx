8bd49a8ab687aea4ad09b631cc6e4ff9aa378f98
Rajpreet0 <rajpreet.singh@gmx.de>
2025-04-10 00:49:10 +0200

    added Time and supabase setup database

--- app/dashboard/timeTracking/page.tsx
+++ app/dashboard/timeTracking/page.tsx
@@ -1,20 +1,56 @@
+'use client'
 import Menu from '@/components/Navigation/Menu'
 import ProtectedRoute from '@/components/ProtectedRoute'
 import SelectPersonCard from '@/components/SelectPersonCard'
 import TimeCard from '@/components/TimeCard'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+
 
 const TimeTracking = () => {
+
+  const [isPlaying, setIsPlaying] = useState(false);
+  const [seconds, setSeconds] = useState(0);
+
+
+  useEffect(() => {
+    let interval: NodeJS.Timeout;
+  
+    if (isPlaying) {
+      interval = setInterval(() => {
+        setSeconds((s) => s + 1);
+      }, 1000);
+    }
+  
+    return () => {
+      if (interval) clearInterval(interval);
+    };
+  }, [isPlaying]);
+
+  const toggleTimer = () => {
+    setIsPlaying((prev) => !prev)
+  }
+
+  const formatTime = (sec: number) => {
+    const hrs = Math.floor(sec / 3600)
+    const mins = Math.floor((sec % 3600) / 60)
+    const secs = sec % 60
+    return [hrs, mins, secs]
+      .map((val) => String(val).padStart(2, '0'))
+      .join(':')
+  }
+
   return (
     <ProtectedRoute>    
         <Menu/>
         <div className='flex flex-col items-center justify-center p-2 min-h-screen gap-8'>
             <div className='flex items-center justify-between w-[30%]'>
                 <h1 className='font-bold text-white text-3xl'>Dashboard</h1>
-                <p className='text-xl text-gray-300'>April 7, 2025</p>
+                <p className='text-xl text-gray-300'>{new Date().toLocaleDateString("de-De", {  year: 'numeric', month: 'long', day: 'numeric' })}</p>
             </div>
             <div className='w-[30%]'>
-              <TimeCard isPlay={true}/>
+              <TimeCard isPlay={isPlaying}             
+                        onToggle={toggleTimer}
+                        time={formatTime(seconds)}/>
             </div>
             <div className='w-[30%]'>
               <SelectPersonCard/>
--- components/TimeCard.tsx
+++ components/TimeCard.tsx
@@ -2,13 +2,15 @@ import { Pause, PlayIcon } from "lucide-react"
 
 interface TimeCardProps {
   isPlay: boolean;
+  onToggle: () => void;
+  time: string;
 }
 
-const TimeCard: React.FC<TimeCardProps> = ({isPlay}) => {
+const TimeCard: React.FC<TimeCardProps> = ({isPlay, onToggle, time }) => {
   return (
     <div className="w-full bg-[#1C1C1C] p-8 rounded-md flex items-center justify-between">
-      <div className="p-2 rounded-md flex items-center gap-4 cursor-pointer hover:scale-105 transition-all" style={{backgroundColor: !isPlay ? "#B80F0A" : "#00C2A8"}}>
-        {!isPlay ? (
+      <div className="p-2 rounded-md flex items-center gap-4 cursor-pointer hover:scale-105 transition-all" style={{backgroundColor: isPlay ? "#B80F0A" : "#00C2A8"}} onClick={onToggle}>
+        {isPlay ? (
           <>
             <Pause color="white" size={20}/>
             <p className="font-bold text-white text-xl">Stop Timer</p>
@@ -21,7 +23,7 @@ const TimeCard: React.FC<TimeCardProps> = ({isPlay}) => {
         )}
       </div>
       <div>
-        <p className="text-white font-bold text-3xl">00:05:14</p>
+        <p className="text-white font-bold text-3xl">{time}</p>
       </div>
     </div>
   )
